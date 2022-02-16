a968644f6c58a49a61dfa64b661598048a07e96b
ggj0418 <ggj0418@naver.com>
2022-02-16 23:18:16 +0900

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,  // 화면 우측 상단에 빨간 띠 없애기
       title: 'LHJ',
       home: Grade(),
     );
   }
-
 }
 
 class Grade extends StatelessWidget {
@@ -31,25 +31,116 @@ class Grade extends StatelessWidget {
       ),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
+          padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: const <Widget>[
-              Text('NAME',
+            children: <Widget>[
+              const Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/monster1.png'),
+                  radius: 60.0, // 이미지 크기 조절
+                ),
+              ),
+              Divider(
+                height: 60.0, // 디바이더의 위아래 위젯의 간격이 60이라는 뜻
+                color: Colors.grey[850],
+                thickness: 0.5,
+                // endIndent: 30.0, // 화면 오른쪽에서부터의 마진
+              ),
+              const Text(
+                'NAME',
                 style: TextStyle(
                   color: Colors.white,
                   letterSpacing: 2.0,
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 10.0,
               ),
-              Text('BBANTO',
+              const Text(
+                'BBANTO',
                 style: TextStyle(
                   color: Colors.white,
                   letterSpacing: 2.0,
                   fontSize: 28.0,
                   fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              const Text(
+                'BBANTO POWER LEVEL',
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              const Text(
+                '14',
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              Row(
+                children: const [
+                  Icon(Icons.check_circle_outline),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text('using lightsaber',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      letterSpacing: 1.0,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: const [
+                  Icon(Icons.check_circle_outline),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text('face hero tattoo',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      letterSpacing: 1.0,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: const [
+                  Icon(Icons.check_circle_outline),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text('fire flames',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      letterSpacing: 1.0,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: const AssetImage('assets/monster1.png'),
+                  radius: 40.0,
+                  backgroundColor: Colors.amber[800],
                 ),
               )
             ],
@@ -58,5 +149,4 @@ class Grade extends StatelessWidget {
       ),
     );
   }
-
 }
