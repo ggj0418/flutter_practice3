230d1eb9d0b83ad4f3275e693d925039c4e9ed4a
ggj0418 <ggj0418@naver.com>
2022-02-16 22:51:35 +0900

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,55 @@ class MyApp extends StatelessWidget {
   // 클래스와 위젯의 관계는 매우 특별함
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'LHJ',
-      // home: Grade(),
-    )
+      home: Grade(),
+    );
+  }
+
+}
+
+class Grade extends StatelessWidget {
+  const Grade({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber[800],
+      appBar: AppBar(
+        title: const Text('Nice'),
+        backgroundColor: Colors.amber[700],
+        centerTitle: true, // 제목을 중앙정렬
+        elevation: 0.0, // 앱 바의 그림자 높이 설정
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const <Widget>[
+              Text('NAME',
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text('BBANTO',
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
 }
